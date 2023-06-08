375122abba6aa1dcc5fb8224309b7b5429b3798c
navsingh3211 <navsingh3211@gmail.com>
2023-06-09 00:55:11 +0530

    login,profile design creation and react component

---
+++ frontend/src/components/profile/Profile.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { motion } from "framer-motion";
+import me from "../../assets/founder.jpg";
+import { Link } from "react-router-dom";
+import {MdDashboard} from "react-icons/md"
+
+const Profile = () => {
+    const options = {
+      initial: {
+        y: "-100%",
+        opacity: 0,
+      },
+      animate: {
+        y: 0,
+        opacity: 1,
+      },
+    };
+    return (
+      <section className="profile">
+        <main>
+          <motion.img src={me} alt="User" {...options} />
+          <motion.h5 {...options} transition={{ delay: 0.3 }}>
+            Navneet Singh
+          </motion.h5>
+          <motion.div {...options} transition={{ delay: 0.5 }}>
+            <Link to="/admin/dashboard" style={{borderRadius:0,backgroundColor:"rgb(40,40,40)"}}><MdDashboard/>DashBoard</Link>
+          </motion.div>
+          <motion.div
+            initial={{ x: "-100vw", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+          >
+            <Link to="/admin/myorders">orders</Link>
+          </motion.div>
+          <motion.button
+            initial={{ x: "-100vw", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{delay:0.3}}
+          >
+            Logout
+          </motion.button>
+        </main>
+      </section>
+    );
+};
+
+export default Profile;
